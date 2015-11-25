ca10262dd6cafcee990f0653b582f4a9dfc61a33
Frank Grivel <frank.grivel@gmail.com>
2015-11-25 09:20:06 +0100

    3.9

--- design/src/design/Hello.java
+++ design/src/design/Hello.java
@@ -1,10 +1,14 @@
 package design;
 
 public class Hello {
-
+	public int vaag;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("iets");
+	}
+	
+	public void iets(){
+		
 	}
 
 }
